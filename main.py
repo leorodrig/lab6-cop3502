e955b182be266f898ea31c1e03f4c0eb92f66c61
leorodrig <leordrig@gmail.com>
2023-10-25 13:50:06 -0400

    main and encode function

---
+++ main.py
@@ -0,0 +1,15 @@
+# lab 6, version control
+
+def main():
+
+    pass
+
+
+def decoder(string_pw):
+    new_val_list = []
+    for val in string_pw:
+        val = int(val)
+        val += 3
+        new_val_list.append(val)
+    decoded_pw = ''.join(new_val_list)
+    return decoded_pw
