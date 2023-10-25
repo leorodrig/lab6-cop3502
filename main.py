5034dfe4c3d032f7df31677400d9f14c0b41982a
leorodrig <leordrig@gmail.com>
2023-10-25 14:39:54 -0400

    added decoder

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 # lab 6, version control
 
 def main():
-    pass
+    # print menu
+    print("""
+    Menu
+    -------------
+    1. Encode
+    2. Decode
+    3. Quit""")
+    # user input and functions
+    running = True
+    while running:
+        user_input = int(input("Please enter an option: "))
+        if user_input == 1:
+            encoded_pw = input("Please enter your password to encode: ")
+            # add encode function here
+        elif user_input == 2:
+            encoded_pw = input("Please enter your password to decode: ")
+            if len(encoded_pw) != 8:
+                quit()
+            password = decoder(encoded_pw)
+
+
+
+
+
+        else:
+            quit()
 
 
 def decoder(string_pw):
     new_val_list = []
     for val in string_pw:
         val = int(val)
-        val += 3
+        val -= 3
         val = str(val)
         new_val_list.append(val)
     decoded_pw = ''.join(new_val_list)
     return decoded_pw
+
+if __name__ == "__main__":
+
